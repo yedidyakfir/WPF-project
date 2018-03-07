14636a2404493bef37c07a47d11369d998ac0f70
yehuda michelson <yehuda.michelson@gmail.com>
2018-03-07 12:15:00 +0200

    k

--- StockPrice/DL/DL.cs
+++ StockPrice/DL/DL.cs
@@ -133,7 +133,7 @@ namespace DL
                 //db.CurrentCoins.RemoveRange(db.CurrentCoins.Where(d => (d.CurrentCoinValueId == "AUD") || (d.CurrentCoinValueId == "CNY")));
                 //db.SaveChanges();
                 //db.Dispose();
-                /*DAL dal = ReadFromXmlFile<DAL>("DataBase");ff
+                /*DAL dal = ReadFromXmlFile<DAL>("DataBase");
                 DB = dal.DB;
                 CurrentCoins = dal.CurrentCoins;
                   foreach (var coin in dal.DB)
