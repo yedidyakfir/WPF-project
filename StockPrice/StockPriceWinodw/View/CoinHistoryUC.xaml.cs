98c2a78ab1bead450c1154fc6c6ff2f891ffa5de
Yedidya Kfir <yedidyakfir@gmail.com>
2018-03-21 01:58:02 +0200

    complete change of the history view,I am amazing no need to thank me

---
+++ StockPrice/StockPriceWinodw/View/CoinHistoryUC.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
+using StockPriceWinodw.ViewModel;
+
+namespace StockPriceWinodw.View
+{
+    /// <summary>
+    /// Interaction logic for CoinHistoryUC.xaml
+    /// </summary>
+    public partial class CoinHistoryUC : UserControl
+    {
+        public CoinHistoryUC()
+        {
+            InitializeComponent();
+            DataContext = new CoinHistoryViewModel();
+        }
+
+        public CoinHistoryUC(string coin)
+        {
+            InitializeComponent();
+            DataContext = new CoinHistoryViewModel(coin, "day");
+            //YAxisName.Text = LineGraph.DataContext.ToString();
+        }
+
+        public void ChangeCoin(string coin, string format = "day")
+        {
+            DataContext = new CoinHistoryViewModel(coin, format);
+        }
+
+        private void UpdateOnclick(object sender, RoutedEventArgs e)
+        {
+            Chart.Update(true);
+        }
+    }
+}
--- StockPrice/StockPriceWinodw/ViewModel/CoinHistoryViewModel.cs
+++ StockPrice/StockPriceWinodw/ViewModel/CoinHistoryViewModel.cs
@@ -6,34 +6,21 @@ using System.Text;
 using System.Threading.Tasks;
 using BL;
 using BE;
+using LiveCharts;
 
 namespace StockPriceWinodw.ViewModel
 {
     internal class CoinHistoryViewModel
     {
         public List<CoinModel> history { get; private set; }
+        private int yAxisSteps = 10;
 
         public CoinHistoryViewModel()
         {
             history = new List<CoinModel>();
-        }
-
-        public double MaxValue
-        {
-            get
-            {
-                return (from c in history
-                        select c.value).Max();
-            }
-        }
-
-        public double MinValue
-        {
-            get
-            {
-                return (from c in history
-                        select c.value).Min();
-            }
+            history.Add(new CoinModel("s", DateTime.Now, 1));
+            history.Add(new CoinModel("s", DateTime.Now - TimeSpan.FromDays(1), 3));
+            history.Add(new CoinModel("s", DateTime.Now - TimeSpan.FromDays(2), 2));
         }
 
         public CoinHistoryViewModel(string coin,string format)
@@ -49,6 +36,64 @@ namespace StockPriceWinodw.ViewModel
             catch(Exception ex)
             {
                 history = new List<CoinModel>();
+            }
+        }
+
+        public float YStepValue
+        {
+            get
+            {
+                return (float)(MaxValue - MinValue) / yAxisSteps;
+            }
+        }
+
+        public double MaxValue
+        {
+            get
+            {
+                if (history.Count == 0)
+                    return 0;
+                return (from c in history
+                        select c.value).Max();
+            }
+        }
+
+        public double MinValue
+        {
+            get
+            {
+                if (history.Count == 0)
+                    return 0;
+                return (from c in history
+                        select c.value).Min();
+            }
+        }
+
+        public double AverageValue
+        {
+            get
+            {
+                if (history.Count == 0)
+                    return 0;
+                return (from c in history
+                        select c.value).Average();
+            }
+        }
+
+        public string[] XValues
+        {
+            get
+            {
+                return history.Select(x => x.lastUpdate.ToString()).ToArray();
+            }
+        }
+
+        public ChartValues<double> YValues
+        {
+            get
+            {
+                return new ChartValues<double>(from c in history
+                                               select c.value);
             }
         }
 
